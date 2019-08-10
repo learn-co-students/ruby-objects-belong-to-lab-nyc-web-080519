18858b1851094d14f63a42fa877fb0c4a64e6e8d
tomjmcnamee <tom@tomjmcnamee.com>
2019-08-10 09:24:17 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,15 @@
+require 'pry'
+require_relative "./artist.rb"
+require_relative "./author.rb"
+require_relative "./song.rb"
+
+
+class Post
+  attr_accessor :title, :author
+
+  def initialize
+
+  end  # ends initialize method
+
+
+end  # Ends Post class
